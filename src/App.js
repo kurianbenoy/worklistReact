19e46255336a2d74be6460444e2616faa7c93d21
kurianbenoy <kurian.pro@gmail.com>
2018-02-02 22:50:00 +0530

    initial rect setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="todowrapper">
+        To Do list APp
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from 'react';;
+
+export default class Header extends React.Component {
+
+  
+}
