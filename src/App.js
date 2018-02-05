9e379a9fd9d0e9a7192ed3af5fadfa532b80d4b0
kurianbenoy <kurian.pro@gmail.com>
2018-02-05 23:59:09 +0530

    Stated with redux and add a input box in redux way

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Todoinput from './components/todoinput'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+      {id 0,text:"make dinner"},
+      {id 1,text :"Fold the laundry."},
+      {id 2, text : "Learn reat"}
+    ],
+    nextId: 3
+  };
+
+  this.addTodo = this.addTodo.bind(this);
+  this.removeTodo = this.removeTodo.bind(this);
+
+  addTodo(todoText) {
+    console.log("Todo added", todotext);
+  }
+
+  removeTodo(id){
+    console.log("1000:",todo);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="todowrapper">
-        JKLDFjlakjfdkladjsf
           <Header />
+          <Todoinput todoText="addTodo(this.addTodo)" />
         </div>
       </div>
     );
--- src/components/todoinput.js
+++ src/components/todoinput.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import './todoinput.css';
 
 export default class Todoinput extends React.Component{
   constructor(props){
     super(props)
+    this.state = {value:"test"};
+
+    this.hadndleChange = this.hadndleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  hadndleChange(e) {
+    console.log("Changed here");
+  }
+
+  addTodo(todo){
+    console.log("TODO",todo);
   }
 
   render (){
-    return {
+    return (
       <div>
         <input type="test" value="" onChange={this.hadndleChange} />
         <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
       </div>
-        );
+      );
     }
   }
